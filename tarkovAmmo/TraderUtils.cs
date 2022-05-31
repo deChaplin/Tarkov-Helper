5b774f8613d86e03e6afa6e84260631cc6cdaaf1
Ahmed Patel <ahmedpatel@synergiesoftware.com>
2022-05-31 10:41:20 +0100

    removed files that shouldnt exist

---
+++ tarkovAmmo/TraderUtils.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+
+namespace tarkovAmmo
+{
+    class trader
+    {
+        public trader(string name, string resetTimeStamp)
+        {
+            this.name = name;
+            this.resetTimeStamp = resetTimeStamp;
+        }
+
+        public string name { get; set; }
+        public string resetTimeStamp { get; set; }
+    }
+}
